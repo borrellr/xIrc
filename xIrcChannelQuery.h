a9869ac15ee3710756aba06e57e9f97da495ef94
borrellr <92683598+borrellr@users.noreply.github.com>
2022-11-22 06:40:56 -0700

    Update xIrcChannelQuery.h
    Revised xIrcChannelQuery.h include file to only contact the global variables needed by channeldialog.ui file.

--- xIrcChannelQuery.h
+++ xIrcChannelQuery.h
@@ -2,27 +2,12 @@
 #define XIRCCHANNELQUERY_H
 #include "channeldialog.h"
 #include "xIrcSocket.h"
+#include "xIrcCommands.h"
 
 #define xCQ_Clear    2
 #define xCQ_Names    3
 #define xCQ_Join     4
 
-class xChannelQuery : public channelDialog
-{
-   Q_OBJECT
-public:
-   xChannelQuery(QWidget *parent = 0, const char *name = 0);
-   ~xChannelQuery();
-   enum  chanResults { Accepted, Rejected, Clear, Names, Join, DccChat };
-
-signals:
-   void ircMessageOut(xIrcMessage *pMsg);
-   void hasResult(int results);
-
-private slots:
-   void names();
-   void dccChat();
-   void joinChannel();
-};
+extern xIrcCommands ircResponses;
 
 #endif
